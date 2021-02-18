937b0cd3e38140a6422de5d4aa8b1301c9642e9f
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2021-02-18 10:14:04 -0800

    make recorder working

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 const path = require("path");
+const fs = require("fs");
+const { EventEmitter } = require("events");
 const { app, BrowserWindow, ipcMain } = require("electron");
 const { run } = require("@elastic/synthetics");
 const unhandled = require("electron-unhandled");
 const debug = require("electron-debug");
+const { chromium } = require("playwright");
 
 unhandled();
 debug();
+
+async function launchContext() {
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+
+  let closingBrowser = false;
+  async function closeBrowser() {
+    if (closingBrowser) return;
+    closingBrowser = true;
+    await browser.close();
+  }
+
+  context.on("page", (page) => {
+    page.on("dialog", () => {});
+    page.on("close", () => {
+      const hasPage = browser
+        .contexts()
+        .some((context) => context.pages().length > 0);
+      if (hasPage) return;
+      closeBrowser().catch((e) => null);
+    });
+  });
+  return { browser, context };
+}
+
+async function openPage(context, url) {
+  const page = await context.newPage();
+  if (url) {
+    if (fs.existsSync(url)) url = "file://" + path.resolve(url);
+    else if (
+      !url.startsWith("http") &&
+      !url.startsWith("file://") &&
+      !url.startsWith("about:")
+    )
+      url = "http://" + url;
+    await page.goto(url);
+  }
+  return page;
+}
 
 // Prevent window from being garbage collected
 let mainWindow;
@@ -27,6 +69,29 @@ async function createWindow() {
     mainWindow = null;
   });
 
+  ipcMain.on("record", async (event) => {
+    const { browser, context } = await launchContext();
+    const actionListener = new EventEmitter();
+    actionListener.on("action", (action) => {
+      console.log("Listening action", action);
+      event.sender.send("action", action);
+    });
+    await context._enableRecorder({
+      launchOptions: {},
+      contextOptions: {},
+      startRecording: true,
+      listener: actionListener,
+    });
+
+    await openPage(context, "https://vigneshh.in");
+
+    ipcMain.on("stop", async (event) => {
+      await browser.close();
+    });
+  });
+  /**
+   * Run Test Journeys
+   */
   ipcMain.on("start", async (event, data) => {
     /**
      * Load journeys
--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 const { ipcRenderer } = require("electron");
 
-const button = document.getElementById("journey");
+const start = document.getElementById("start");
+const record = document.getElementById("record");
+const stop = document.getElementById("stop");
 const results = document.getElementById("results");
+const records = document.getElementById("records");
 
-button.addEventListener("click", () => {
+start.addEventListener("click", () => {
   ipcRenderer.on("done", (event, data) => {
-    results.innerHTML = JSON.stringify(data);
+    results.innerText = JSON.stringify(data);
   });
   ipcRenderer.send("start", true);
+});
+
+record.addEventListener("click", () => {
+  const tBody = document.createElement("tbody");
+  records.appendChild(tBody);
+  ipcRenderer.on("action", (event, action) => {
+    /**
+     * Render in table
+     */
+    const { name, url, selector, text } = action.action;
+    const tRow = tBody.insertRow();
+    tRow.insertCell().appendChild(document.createTextNode(name));
+    if (url || selector) {
+      tRow.insertCell().appendChild(document.createTextNode(url || selector));
+    }
+    if (text) {
+      tRow.insertCell().appendChild(document.createTextNode(text));
+    }
+  });
+  ipcRenderer.send("record", true);
+});
+
+stop.addEventListener("click", () => {
+  ipcRenderer.send("stop");
 });
