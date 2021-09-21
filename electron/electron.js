be44520f1474710a74202c244d25bf8c62f1d108
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2021-09-21 15:30:47 -0700

    feat: build menu for distribution (#17)
    * feat: build menu for distribution
    
    * apply suggestions from code review
    
    Co-authored-by: Justin Kambic <justin.kambic@elastic.co>
    
    Co-authored-by: Justin Kambic <justin.kambic@elastic.co>

--- electron/electron.js
+++ electron/electron.js
@@ -1,10 +1,11 @@
 const { join } = require("path");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 const isDev = require("electron-is-dev");
 const unhandled = require("electron-unhandled");
 const debug = require("electron-debug");
 const initAutoUpdate = require("update-electron-app");
 const setupListeners = require("./execution");
+const buildMenu = require("./menu");
 
 // For dev
 unhandled();
@@ -20,7 +21,6 @@ const BUILD_DIR = join(__dirname, "..", "build");
 
 async function createWindow() {
   const win = new BrowserWindow({
-    title: "Synthetics Recorder",
     width: 1100,
     height: 700,
     minHeight: 500,
@@ -37,9 +37,15 @@ async function createWindow() {
     : win.loadFile(join(BUILD_DIR, "index.html"));
 }
 
+function createMenu() {
+  const menuTemplate = buildMenu(app.name);
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
+}
+
 app.on("ready", () => {
   createWindow();
   setupListeners();
+  createMenu();
 });
 
 app.on("window-all-closed", () => {
