8b9145a9d0672b1a8983de1cc02ffc89fcd542a4
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2021-11-30 12:00:31 -0800

    use header package for linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,36 @@
-/**
- * MIT License
- *
- * Copyright (c) 2021-present, Elastic NV
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- */
+/*
+MIT License
 
-/* eslint-disable @typescript-eslint/no-var-requires */
+Copyright (c) 2021-present, Elastic NV
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 const { readFileSync } = require("fs");
-
 const LICENSE = readFileSync("./LICENSE", "utf-8");
-const LICENSE_HEADER =
-  "/**\n" +
-  LICENSE.split("\n")
-    .map(line => ` *${line ? ` ${line}` : ""}`)
-    .join("\n") +
-  "\n */";
+const LICENSE_HEADER = "\n" + LICENSE;
 
 module.exports = {
   env: {
     es2021: true,
   },
+  plugins: ["header"],
   extends: ["react-app", "plugin:react/recommended"],
   ignorePatterns: ["build", "local-browsers"],
   overrides: [
@@ -60,11 +53,6 @@ module.exports = {
   ],
   rules: {
     "default-case": 0,
-    "require-license-header": [
-      "error",
-      {
-        license: LICENSE_HEADER,
-      },
-    ],
+    "header/header": [2, "block", LICENSE_HEADER],
   },
 };
