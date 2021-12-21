2937da839f12f790748112673e9d2a05b40db027
Justin Kambic <jk@elastic.co>
2021-12-21 12:00:07 -0500

    Add setup file for unit tests, define `window.require`.

--- jest.unit.setup.js
+++ jest.unit.setup.js
@@ -22,8 +22,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-module.exports = {
-  preset: "ts-jest",
-  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `e2e`],
-  setupFilesAfterEnv: ["./jest.unit.setup.js"],
-};
+/**
+ * We do this because there are numerous places in the frontend code that
+ * need to call `window.require("electron-better-ipc"). Because the tests
+ * are not configured to run with node env features, `window.require` will
+ * not be defined.
+ *
+ * We rely on e2e testing to ensure that our communications via IPC to the
+ * electron process are working, so for the most part we can ignore this
+ * feature, and mock its responses when testing frontend functionality in
+ * unit tests. This setup will preclude the need to include the declaration
+ * of `require` on the `window` object before any test that depends on this.
+ */
+
+window.require = require;
