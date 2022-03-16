4a574b251f19da05d1b49c72fb84b952ca32092e
kyungeunni <kyungeunni@gmail.com>
2022-03-16 12:35:26 +0100

    fix: download ffmpeg browsers along with chromium

--- scripts/download-browsers.js
+++ scripts/download-browsers.js
@@ -29,33 +29,53 @@ const path = require("path");
 const {
   downloadBrowserWithProgressBar,
 } = require("playwright/lib/utils/browserFetcher");
+const SYNTHETICS_BROWSER_REVISIONS = require("@elastic/synthetics/node_modules/playwright-chromium/browsers.json");
 const { getChromeVersion } = require("./install-pw");
-
+const getFfmpegVersion = () => {
+  const { browsers } = SYNTHETICS_BROWSER_REVISIONS;
+  const revision = browsers.find(browser => browser.name === "ffmpeg").revision;
+  return revision;
+};
 const EXECUTABLE_PATHS = {
-  linux: ["chrome-linux", "chrome"],
-  mac: ["chrome-mac", "Chromium.app", "Contents", "MacOS", "Chromium"],
-  win: ["chrome-win", "chrome.exe"],
+  chromium: {
+    linux: ["chrome-linux", "chrome"],
+    mac: ["chrome-mac", "Chromium.app", "Contents", "MacOS", "Chromium"],
+    win: ["chrome-win", "chrome.exe"],
+  },
+  ffmpeg: {
+    linux: ["ffmpeg-linux"],
+    mac: ["ffmpeg-mac"],
+    win: ["ffmpeg-win64.exe"],
+  },
 };
 
-const CHROMIUM_DOWNLOAD_URLS = {
-  linux: "%s/builds/chromium/%s/chromium-linux.zip",
-  mac: "%s/builds/chromium/%s/chromium-mac.zip",
-  win: "%s/builds/chromium/%s/chromium-win64.zip",
-  // 'mac-arm64': '%s/builds/chromium/%s/chromium-mac-arm64.zip',
+const DOWNLOAD_URLS = {
+  chromium: {
+    linux: "%s/builds/chromium/%s/chromium-linux.zip",
+    mac: "%s/builds/chromium/%s/chromium-mac.zip",
+    win: "%s/builds/chromium/%s/chromium-win64.zip",
+    // 'mac-arm64': '%s/builds/chromium/%s/chromium-mac-arm64.zip',
+  },
+  ffmpeg: {
+    linux: "%s/builds/ffmpeg/%s/ffmpeg-linux.zip",
+    mac: "%s/builds/ffmpeg/%s/ffmpeg-mac.zip",
+    win: "%s/builds/ffmpeg/%s/ffmpeg-win64.zip",
+    // 'mac-arm64': '%s/builds/ffmpeg/%s/ffmpeg-mac.zip',
+  },
 };
 
-async function download(platform, revision, directory) {
-  const executablePath = findExecutablePath(directory, platform);
+async function download(platform, browser, revision, directory) {
+  const executablePath = findExecutablePath(directory, browser, platform);
   const downloadHost =
     process.env["PLAYWRIGHT_DOWNLOAD_HOST"] ||
     "https://playwright.azureedge.net";
   const downloadURL = util.format(
-    CHROMIUM_DOWNLOAD_URLS[platform],
+    DOWNLOAD_URLS[browser][platform],
     downloadHost,
     revision
   );
-  const title = `${platform} v${revision}`;
-  const downloadFileName = `playwright-download-chromium-${platform}-${revision}.zip`;
+  const title = `${browser} v${revision} for ${platform}`;
+  const downloadFileName = `playwright-download-${browser}-${platform}-${revision}.zip`;
   try {
     // eslint-disable-next-line no-console
     console.info("Downloading browser ", title);
@@ -71,23 +91,35 @@ async function download(platform, revision, directory) {
   }
 }
 
-function findExecutablePath(dir, platform) {
-  const tokens = EXECUTABLE_PATHS[platform];
+function findExecutablePath(dir, browser, platform) {
+  const tokens = EXECUTABLE_PATHS[browser][platform];
   return tokens ? path.join(dir, ...tokens) : undefined;
 }
 
 exports.downloadForPlatform = async function downloadForPlatform(platform) {
   platform = translatePlatform(platform);
-  const [, revision] = getChromeVersion().split("-");
+  const [, chromeRevision] = getChromeVersion().split("-");
+  const ffmpegVersion = getFfmpegVersion();
   const directory = path.join(
     process.cwd(),
     "local-browsers",
     "_releases",
-    `${getChromeVersion()}-${platform}`
+    platform,
+    getChromeVersion()
+  );
+  const ffmpegDir = path.join(
+    process.cwd(),
+    "local-browsers",
+    "_releases",
+    platform,
+    `ffmpeg-${ffmpegVersion}`
   );
   try {
-    await download(platform, revision, directory);
+    await download(platform, "chromium", chromeRevision, directory);
+    await download(platform, "ffmpeg", ffmpegVersion, ffmpegDir);
   } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
     throw Error("Failed to download browser for platform " + platform);
   }
 };
