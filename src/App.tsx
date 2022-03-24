f5c053e74aa08cb8bb536ab075d1f69211dfa701
Justin Kambic <jk@elastic.co>
2022-03-24 15:57:51 -0400

    Remove unneeded script from package.json.

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-
 import React, { useContext } from "react";
 import { useEffect, useState } from "react";
 import { EuiCode, EuiEmptyPrompt, EuiProvider } from "@elastic/eui";
