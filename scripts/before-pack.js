27cf388e50b7408dfe8f4fd6d15f3293980169c4
Kyung-eun Kim <kyungeunni@gmail.com>
2022-04-06 16:28:01 +0200

    chore: Download browsers for all platforms to release builds from single machine (#165)
    * chore: move electron-builder config to yml file
    
    * chore: configure electron-builder to include platform-specific browser to resources
    
    * feat: add script downloading all browsers
    
    * fix: extra install steps for sharp(native dep)
    
    * fix: assign parent process.env when spawning
    
    * chore: add option for linux build when publishing
    
    * fix: chmod resources dir when building for linux
    
    * chore: remove Appimage from linux target
    
    * chore: remove ffmpeg installer
    
    * fix: specify correct directory for browser download

---
+++ scripts/before-pack.js
@@ -0,0 +1,54 @@
+/*
+MIT License
+
+Copyright (c) 2021-present, Elastic NV
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+const { spawn } = require("child_process");
+const { Arch } = require("electron-builder");
+const { downloadForPlatform } = require("./download-chromium");
+
+exports.default = function beforePack(ctx) {
+  const arch = Arch[ctx.arch];
+  const platform = ctx.electronPlatformName;
+  return Promise.all([downloadForPlatform(platform), fixSharp(arch, platform)]);
+};
+
+function fixSharp(arch, platform) {
+  return new Promise((resolve, reject) => {
+    const npmInstall = spawn("npm", ["run", "fix-sharp"], {
+      stdio: "inherit",
+      shell: true,
+      env: {
+        ...process.env,
+        npm_config_arch: arch,
+        npm_config_platform: platform,
+      },
+    });
+    npmInstall.on("close", code => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error("process finished with error code " + code));
+      }
+    });
+    npmInstall.on("error", reject);
+  });
+}
