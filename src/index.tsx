81286dd279d736e12ef66fef22b5f91b44e10b4a
Justin Kambic <jk@elastic.co>
2022-04-26 15:32:58 -0400

    Upgrade react and RTL.

--- src/index.tsx
+++ src/index.tsx
@@ -23,12 +23,17 @@ THE SOFTWARE.
 */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const element = document.getElementById('root');
+
+if (element) {
+  const root = ReactDOMClient.createRoot(element);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
