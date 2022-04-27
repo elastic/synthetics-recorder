58e2ffe3660aed989af30084f2637c78dae8e860
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-04-27 11:15:04 +0100

    ci: copy files to the workspace

--- .ci/scripts/run-test.sh
+++ .ci/scripts/run-test.sh
@@ -8,6 +8,17 @@ echo $USER
 echo $USERNAME
 
 source $NVM_DIR/nvm.sh
+dir=$(pwd)
 cd $DOCKER_BASE_DIR
 nvm use
 NPM_CONFIG_LOGLEVEL=verbose npm test
+
+# CI specific requirements
+# Copy the junit files to the workspace to be shared with the
+# pipeline workspace
+cleanup() {
+  cd $DOCKER_BASE_DIR
+  cp ./*junit.xml "${dir}/"
+}
+
+trap cleanup EXIT
