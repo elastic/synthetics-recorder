93038eca91eaa34ca055386cf402ab065b16e954
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-04-27 13:55:40 +0100

    detect TTY

--- .ci/scripts/run-test-in-docker.sh
+++ .ci/scripts/run-test-in-docker.sh
@@ -8,8 +8,15 @@ if [[ "$(docker images -q $DOCKER_IMAGE 2> /dev/null)" == "" ]]; then
   docker pull $DOCKER_IMAGE
 fi
 
+DOCKER_RUN_OPTIONS="-i --rm"
+
+# Only allocate tty if we detect one
+if [ -t 0 ] && [ -t 1 ]; then
+    DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS -t"
+fi
+
 docker run \
-  --rm \
+  $DOCKER_RUN_OPTIONS \
   -u '0:0' \
   -v "$(pwd):/synthetics-recorder" \
   $DOCKER_IMAGE \
