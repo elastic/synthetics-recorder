bc3d033e685d6b39b3453351b20b1aef5786b619
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-05-12 12:40:42 +0100

    Revert "Use macosx workers and load the keychain on the fly"
    This reverts commit d821132609a50a24a4e0720f806a86825d15d483.

--- .ci/scripts/release-ci.sh
+++ .ci/scripts/release-ci.sh
@@ -6,12 +6,6 @@ echo 'set env context'
 export HOME='/var/lib/jenkins'
 export CSC_KEYCHAIN="/var/lib/jenkins/Library/Keychains/Elastic.keychain-db"
 
-echo 'if keychain does not exist'
-if [ ! -f $CSC_KEYCHAIN ] ; then
-  set +x
-  echo "${KEYCHAIN_DB}" | base64 --decode > ${CSC_KEYCHAIN}
-fi
-
 echo 'set keychain'
 set -x
 security list-keychains -s "$CSC_KEYCHAIN"
