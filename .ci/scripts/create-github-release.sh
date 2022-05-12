6580c819d671f5adae79ef5289f27d7022dbfad1
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-05-12 13:05:11 +0100

    withGh does not work in workers without arch labels

--- .ci/scripts/create-github-release.sh
+++ .ci/scripts/create-github-release.sh
@@ -2,6 +2,16 @@
 set -eox pipefail
 
 ls -l ${DIST_FOLDER}/*.* || true
+
+if ! gh --version &>/dev/null ; then
+  ## install gh
+  wget -q https://github.com/cli/cli/releases/download/v2.4.0/gh_2.4.0_macOS_amd64.tar.gz -O gh.tar.gz
+  tar -xpf gh.tar.gz --strip-components=2
+  PATH="$(pwd):${PATH}"
+  export PATH
+  gh --version
+fi
+
 gh release \
   create \
   "${TAG_NAME}" \
