ac61186b99377604d3ef9d620fa96a9ded77f686
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-05-12 10:45:16 +0100

    more test

--- .ci/scripts/release-ci.sh
+++ .ci/scripts/release-ci.sh
@@ -2,8 +2,15 @@
 
 set -eo pipefail
 
+echo 'set env context'
+export HOME='/var/lib/jenkins'
+export OLDPATH="${PATH}"
+export PATH="/var/lib/jenkins/.pyenv/plugins/pyenv-virtualenv/shims:/var/lib/jenkins/.pyenv/shims:/var/lib/jenkins/.pyenv/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 echo 'debug'
 set -x
+echo "SHELL=${SHELL}"
+echo "USER=${USER}"
+echo "PWD=${PWD}"
 security list-keychains || true
 security list-keychains -d user || true
 security list-keychains -d system || true
@@ -11,8 +18,8 @@ security list-keychains -d common || true
 security list-keychains -d dynamic || true
 
 echo 'set keychain for the user'
-security list-keychains -d user -s /var/lib/jenkins/Library/Keychains/Elastic.keychain-db || true
-security list-keychains || true
+security list-keychains -v -d user -s /var/lib/jenkins/Library/Keychains/Elastic.keychain-db || true
+security list-keychains -v || true
 CSC_KEYCHAIN="/var/lib/jenkins/Library/Keychains/Elastic.keychain-db"
 export CSC_KEYCHAIN
 
@@ -35,5 +42,6 @@ security add-generic-password -U -a test-jenkins -s test-jenkins -w test-jenkins
 security find-generic-password -a test-jenkins -s test-jenkins -w "$CSC_KEYCHAIN"
 
 echo 'run release-ci'
+export PATH="${OLDPATH}"
 npm ci
 npm run release-ci
