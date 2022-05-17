735706e75c518d8c3b2c0cd4107f4bed450cbdfb
Justin Kambic <jk@elastic.co>
2022-05-17 11:52:17 -0400

    Remove unused import.

--- src/common/shared.ts
+++ src/common/shared.ts
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-import type { Action, Step, Steps } from '@elastic/synthetics';
+import type { Action, Steps } from '@elastic/synthetics';
 import { RendererProcessIpc } from 'electron-better-ipc';
 import React from 'react';
 import type { ActionContext, Journey, JourneyType } from '../../common/types';
