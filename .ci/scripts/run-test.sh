2a89d6006bdd123b369540bd139f45f030538df5
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-11-03 18:59:41 +0000

    use https://github.com/nodejs/docker-node/blob/main/docs/BestPractices.md\#global-npm-dependencies

--- .ci/scripts/run-test.sh
+++ .ci/scripts/run-test.sh
@@ -11,10 +11,6 @@ echo $USERNAME
 set -x
 # If NPM_COMMAND then run it.
 if [ -n "${NPM_COMMAND}" ] ; then
-  # Change the path otherwise it will fail in docker as it tries to
-  # to use /root/.npm
-  mkdir -p /synthetics-recorder/.npm || true
-  npm config --global set prefix /synthetics-recorder/.npm
   npm ${NPM_COMMAND}
 fi
 NPM_CONFIG_LOGLEVEL=verbose npm run test
