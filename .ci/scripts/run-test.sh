4ff23eb906da35d2c734a719d7f945a9e2ce9f4e
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-11-04 10:14:54 +0000

    chore: for debugging purposes
    fix permissions

--- .ci/scripts/run-test.sh
+++ .ci/scripts/run-test.sh
@@ -3,14 +3,19 @@ set -eo pipefail
 
 Xvfb ${DISPLAY} -screen 0 1024x768x16 &
 
-echo $UID
-echo $GID
-echo $USER
-echo $USERNAME
-
 set -x
+
+# For debugging purposes
+whoami
+pwd
+echo $HOME
+ls -ltra $HOME
+
 # If NPM_COMMAND then run it.
 if [ -n "${NPM_COMMAND}" ] ; then
+  # for some reason this folder got permission denied when
+  # running on the CI workers Installation error: EACCES: permission denied
+  mkdir -p $HOME/.npm || true
   npm ${NPM_COMMAND}
 fi
 NPM_CONFIG_LOGLEVEL=verbose npm run test
