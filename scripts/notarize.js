4fc650543b87b443e497474e949e20e36d21e622
kyungeunni <kyungeunni@gmail.com>
2022-12-23 13:44:27 +0900

    fix: import correct module for notarizing

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-const { notarize } = require('electron-notarize');
+const { notarize } = require('@electron/notarize');
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
