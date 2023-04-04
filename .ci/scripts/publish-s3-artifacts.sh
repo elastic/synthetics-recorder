b952218f2b456bc07396045d9dc55cebd5c29ef4
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-04-04 21:27:33 +0200

    for debugging purposes

--- .ci/scripts/publish-s3-artifacts.sh
+++ .ci/scripts/publish-s3-artifacts.sh
@@ -5,6 +5,7 @@ DIST_LOCATION=$1
 
 ls -l "${DIST_LOCATION}"/*.* || true
 
+echo "Publish artifacts to S3"
 cd "${DIST_LOCATION}"
 docker run \
   -e AWS_ACCESS_KEY_ID \
@@ -12,3 +13,10 @@ docker run \
   -v "$(pwd)":/root \
   docker.elastic.co/infra/aws-cli:latest \
     aws s3 cp ./*.* s3://download.elasticsearch.org/synthetics-recorder/
+
+echo "List artifacts from S3"
+docker run \
+  -e AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY \
+  docker.elastic.co/infra/aws-cli:latest \
+    aws s3 ls s3://download.elasticsearch.org/synthetics-recorder/
