9770e4c77457f8e65d53f661fe675b438af37b10
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-04-27 20:12:57 +0200

    ci: use /root and add debug traces

--- .ci/scripts/publish-s3-artifacts.sh
+++ .ci/scripts/publish-s3-artifacts.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
-set -eox pipefail
+set -eo pipefail
 
 DIST_LOCATION=$1
 
+echo "> List artifacts in the dist folder ($DIST_LOCATION)"
 ls -l "${DIST_LOCATION}"/*.* || true
 
-echo "Publish artifacts to S3"
+echo "> Publish artifacts to S3"
 cd "${DIST_LOCATION}"
 for artifact in ./*; do
+  echo ">> publishing $artifact ..."
   docker run \
     -e AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY \
     -v "$(pwd)":/root \
     docker.elastic.co/infra/aws-cli:latest \
-    aws s3 cp $artifact s3://download.elasticsearch.org/synthetics-recorder/
+    aws s3 cp /root/"$artifact" s3://download.elasticsearch.org/synthetics-recorder/
 done
 
-echo "List artifacts from S3"
+echo "> List artifacts from S3"
 docker run \
   -e AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY \
