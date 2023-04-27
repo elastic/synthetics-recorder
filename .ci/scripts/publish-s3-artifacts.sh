584ea08138d3c8abc8040860553679579558f1d9
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-04-27 22:15:19 +0200

    remove hardcoded

--- .ci/scripts/publish-s3-artifacts.sh
+++ .ci/scripts/publish-s3-artifacts.sh
@@ -2,9 +2,14 @@
 set -eo pipefail
 
 DIST_LOCATION=$1
+DOCKER_IMAGE=docker.elastic.co/infra/aws-cli:latest
+S3_PATH=s3://download.elasticsearch.org/synthetics-recorder/
 
 echo "> List artifacts in the dist folder ($DIST_LOCATION)"
 ls -l "${DIST_LOCATION}"/*.* || true
+
+echo "Fetch docker image"
+docker pull $DOCKER_IMAGE --quiet
 
 echo "> Publish artifacts to S3"
 cd "${DIST_LOCATION}"
@@ -14,13 +19,13 @@ for artifact in ./*; do
     -e AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY \
     -v "$(pwd)":/root \
-    docker.elastic.co/infra/aws-cli:latest \
-    aws s3 cp /root/"$artifact" s3://download.elasticsearch.org/synthetics-recorder/
+    $DOCKER_IMAGE \
+    aws s3 cp /root/"$artifact" $S3_PATH
 done
 
 echo "> List artifacts from S3"
 docker run \
   -e AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY \
-  docker.elastic.co/infra/aws-cli:latest \
-    aws s3 ls s3://download.elasticsearch.org/synthetics-recorder/
+  $DOCKER_IMAGE \
+  aws s3 ls $S3_PATH
