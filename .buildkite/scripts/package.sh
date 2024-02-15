1960bb11896c57fbff5c94784c8106e4aaea5729
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-02-15 22:38:04 +0100

    sign linux gpg and skip notarize

--- .buildkite/scripts/package.sh
+++ .buildkite/scripts/package.sh
@@ -14,4 +14,24 @@ npm ci
 # Disable signing
 # see https://www.electron.build/code-signing#how-to-disable-code-signing-during-the-build-process-on-macos
 export CSC_IDENTITY_AUTO_DISCOVERY=false
+# Disable notarize, see scripts/notarize.js
+export SKIP_NOTARIZATION=true
 npm run release-ci
+
+# Store unsigned artifacts
+if [ -n "$BUILDKITE" ] ; then
+    mv dist artifacts-to-sign
+    # (only *nix)
+    # This is the contract with the unified-release-gpg-signing pipeline
+    buildkite-agent artifact upload "artifacts-to-sign/*.deb;artifacts-to-sign/*.dmg;artifacts-to-sign/*.zip"
+
+    mv artifacts-to-sign win-artifacts-to-sign
+    # (only windows)
+    # TODO: need to clarify the contract.
+    buildkite-agent artifact upload "win-artifacts-to-sign/*.exe"
+
+    mv win-artifacts-to-sign mac-artifacts-to-sign
+    # (only mac)
+    # TODO: need to clarify the contract.
+    buildkite-agent artifact upload "mac-artifacts-to-sign/*.dmg"
+fi
