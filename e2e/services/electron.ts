fca67c6a1ec19b38562ec6374c0c28d9a397435d
Justin Kambic <jk@elastic.co>
2024-11-13 15:00:23 -0500

    Assert binary exists.

--- e2e/services/electron.ts
+++ e2e/services/electron.ts
@@ -23,6 +23,7 @@ THE SOFTWARE.
 */
 
 import { _electron, ElectronApplication, Page } from 'playwright-core';
+import fs from 'fs';
 import { TestBrowserService } from './browser';
 import path from 'path';
 import { env } from '.';
@@ -34,13 +35,13 @@ export class ElectronServiceFactory {
   async getInstance() {
     if (this.#instance) return this.#instance;
 
+    const binPath = path.join(__dirname, '..', '..', 'build', 'electron', 'electron.js');
+
+    if (!fs.existsSync(binPath)) throw Error('Electron binary not found');
+
     try {
       this.#instance = await _electron.launch({
-        args: [
-          path.join(__dirname, '..', '..', 'build', 'electron', 'electron.js'),
-          '--no-sandbox',
-          '--enable-logging',
-        ],
+        args: [binPath, '--no-sandbox', '--enable-logging'],
         env: {
           DISPLAY: env.DISPLAY,
           /* we are casting this as a string to satisfy the `launch` params requirement,
