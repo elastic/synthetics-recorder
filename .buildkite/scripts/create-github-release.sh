ec78d17aeeb21245f8f62d76b8a2bcccea571110
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-12-18 19:25:45 +0100

    avoid interactive approach

--- .buildkite/scripts/create-github-release.sh
+++ .buildkite/scripts/create-github-release.sh
@@ -38,9 +38,9 @@ if [ -n "${BUILDKITE_TAG}" ] ; then
     create \
     "${BUILDKITE_TAG}" \
     --draft \
-    --title "${BUILDKITE_TAG}" \
-    --repo elastic/synthetics-recorder \
-    ./$DIST_LOCATION/*.*
+    --generate-notes \
+    --repo "elastic/synthetics-recorder" \
+    "${DIST_LOCATION}"/*.*
 else
   echo "gh release won't be triggered this is not a Git tag release, but let's list the releases"
   # VAULT_GITHUB_TOKEN is the GitHub ephemeral token created in Buildkite
